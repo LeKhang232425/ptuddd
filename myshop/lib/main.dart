3f9e4e10c0e2f12f972b6fd75c22fcb61401d262
khang000 <hoangkhang2324@gmail.com>
2022-09-15 23:36:14 +0700

    Xay dung trang cac san pham nguoi dung

--- myshop/lib/main.dart
+++ myshop/lib/main.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:myshop/ui/products/product_overview_screen.dart';
+import 'package:myshop/models/product.dart';
 import 'ui/products/products_manager.dart';
 import 'ui/products/product_detail_screen.dart';
 import 'ui/products/product_overview_screen.dart';
+import 'ui/products/user_products_screen.dart';
+
 void main() {
   runApp(const MyApp());
 }
@@ -23,10 +25,8 @@ class MyApp extends StatelessWidget {
             secondary: Colors.deepOrange,
           ),
         ),
-        home: SafeArea(
-          child: ProductsOverviewScreen(),
-        ),
-        
-      );
+        home: const SafeArea(
+          child: UserProductsScreen(),
+        ));
   }
 }

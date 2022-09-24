6b83a34eec63d5460dfbca9b723909cd24a25f62
khangb1910079 <hoangkhang2324@gmail.com>
2022-09-24 15:19:18 +0700

    Them thong tin san pham trong gio hang

--- myshop/lib/ui/products/product_overview_screen.dart
+++ myshop/lib/ui/products/product_overview_screen.dart
@@ -3,6 +3,8 @@ import '../shared/app_drawer.dart';
 import '../cart/cart_screen.dart';
 import 'products_gird.dart';
 import '../shared/app_drawer.dart';
+import 'top_right_badge.dart';
+import '../cart/cart_manager.dart';
 
 
 enum FilterOptions { favorites, all }
@@ -33,13 +35,16 @@ class _ProductsOverviewScreenState extends State<ProductsOverviewScreen> {
   }
 
   Widget buildShoppingCartIcon() {
-    return IconButton(
-      icon: const Icon(
-        Icons.shopping_cart,
+    return TopRightBadge(
+      data: CartManager().productCount,
+      child: IconButton(
+        icon: const Icon(
+          Icons.shopping_cart,
+        ),
+        onPressed: () {
+          Navigator.of(context).pushNamed(CartScreen.routeName);
+        },
       ),
-      onPressed: () {
-       Navigator.of(context).pushNamed(CartScreen.routeName);
-      },
     );
   }
 
